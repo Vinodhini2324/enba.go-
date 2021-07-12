c7a2033b83258f437b3ed86467471dfc2aad4567
Vinodhini2324 <86877127+Vinodhini2324@users.noreply.github.com>
2021-07-12 12:44:37 +0530

    enba.go

---
+++ enba.go
@@ -0,0 +1,15 @@
+package main
+
+import "https://github.com/gin-gonic"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() 
+	}
+  
+  
